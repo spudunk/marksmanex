df30190d4324abf11ddabcd853a849ab64e6128f
Christopher Hicks <hickscme@gmail.com>
2024-01-12 13:59:15 -0800

    load hero image using <img />

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,12 @@
 <script lang="ts">
-  import Google from "$lib/icons/google.svg";
-  import Facebook from "$lib/icons/facebook.svg";
-  import Yelp from "$lib/icons/yelp_burst.svg";
   import MetaTags from "$lib/SEO/MetaTags.svelte";
   import LdTag from "$lib/SEO/LDTag.svelte";
   import Contact from "$lib/Contact.svelte";
   import ReviewLinks from "$lib/ReviewLinks.svelte";
-  import { site, organizationSchema, websiteSchema, galleryImages } from "$lib";
   import Navbar from "$lib/Navbar.svelte";
   import Carousel from "$lib/Carousel.svelte";
-
+  import { site, organizationSchema, websiteSchema, galleryImages } from "$lib";
+  // import heroImage from "$lib/hero.jpg";
 </script>
 
 <MetaTags />
@@ -18,7 +15,11 @@
 
 <svelte:head>
   <!-- Preload Hero image for Lighthouse Performance -->
-  <link rel="preload" as="image" href="https://marksmanexteriors.com/cdn-cgi/imagedelivery/XvH0UEoGmg1LgCBcC8XRgw/8d1dfa22-0e73-40e2-2edf-3218fede6400/public">
+  <link
+    rel="preload"
+    as="image"
+    href="https://marksmanexteriors.com/cdn-cgi/imagedelivery/XvH0UEoGmg1LgCBcC8XRgw/8d1dfa22-0e73-40e2-2edf-3218fede6400/public"
+  />
 </svelte:head>
 
 <Navbar hero />
@@ -27,9 +28,15 @@
     id="hero"
     class="-z-10 relative min-h-[90vh] flex flex-col justify-center"
   >
-    <div
-      class="absolute top-0 left-0 bottom-0 right-0 overflow-x-clip bg-frontDoor bg-cover bg-center"
-    ></div>
+    <img
+      alt="luxury home with new windows"
+      src="https://marksmanexteriors.com/cdn-cgi/imagedelivery/XvH0UEoGmg1LgCBcC8XRgw/8d1dfa22-0e73-40e2-2edf-3218fede6400/public"
+      srcset="https://marksmanexteriors.com/cdn-cgi/imagedelivery/XvH0UEoGmg1LgCBcC8XRgw/8d1dfa22-0e73-40e2-2edf-3218fede6400/w=480 480w, https://marksmanexteriors.com/cdn-cgi/imagedelivery/XvH0UEoGmg1LgCBcC8XRgw/8d1dfa22-0e73-40e2-2edf-3218fede6400/w=800 800w, https://marksmanexteriors.com/cdn-cgi/imagedelivery/XvH0UEoGmg1LgCBcC8XRgw/8d1dfa22-0e73-40e2-2edf-3218fede6400/public 1080w"
+      sizes="100vw"
+      class="absolute top-0 left-0 bottom-0 right-0 min-w-full min-h-full object-cover"
+      width="1080"
+      height="732"
+    />
     <div
       class="mix-blend-multiply backdrop-blur opacity-60 absolute top-0 left-0 bottom-0 right-0 bg-gradient-to-b from-neutral-500 via-neutral-950 to-neutral-500"
     ></div>
