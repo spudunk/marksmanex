21bf2c6ad0953283b304f98c1c0cb2299377a0e8
Christopher Hicks <hickscme@gmail.com>
2024-01-17 10:36:08 -0800

    move head tags to layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,7 +4,32 @@
   import "@fontsource/noto-serif";
   import "@fontsource/bebas-neue";
   import Footer from "$lib/Footer.svelte";
+  import { site } from "$lib";
 </script>
+
+<svelte:head>
+  <!--  Favicon & WebManifest -->
+
+  <link rel="icon" href={`${site.url}favicon.ico`} />
+  <link
+    rel="apple-touch-icon"
+    sizes="180x180"
+    href={`${site.url}favicon/apple-touch-icon.png`}
+  />
+  <link
+    rel="icon"
+    type="image/png"
+    sizes="32x32"
+    href={`${site.url}favicon/favicon-32x32.png`}
+  />
+  <link
+    rel="icon"
+    type="image/png"
+    sizes="16x16"
+    href={`${site.url}favicon/favicon-16x16.png`}
+  />
+  <link rel="manifest" href={`${site.url}site.webmanifest`} />
+</svelte:head>
 
 <slot />
 <Footer />
