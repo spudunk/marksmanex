a3c2077a2102ea3011296af508ee7e6c8a616588
Christopher Hicks <hickscme@gmail.com>
2024-01-18 13:31:30 -0800

    update manifest

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -10,25 +10,26 @@
 <svelte:head>
   <!--  Favicon & WebManifest -->
 
-  <link rel="icon" href={`${site.url}favicon.ico`} />
-  <!-- <link
-    rel="apple-touch-icon"
-    sizes="180x180"
-    href={`${site.url}favicon/apple-touch-icon.png`}
+  <link rel="manifest" href={`${site.url}/site.webmanifest`} />
+  <link rel="icon" href={`${site.url}/favicon.ico`} />
+  <link
+  rel="apple-touch-icon"
+  sizes="180x180"
+  href={`${site.url}/favicon/apple-touch-icon.png`}
   />
+  
   <link
     rel="icon"
     type="image/png"
     sizes="32x32"
-    href={`${site.url}favicon/favicon-32x32.png`}
+    href={`${site.url}/favicon/favicon-32x32.png`}
   />
   <link
     rel="icon"
     type="image/png"
     sizes="16x16"
-    href={`${site.url}favicon/favicon-16x16.png`}
-  /> -->
-  <link rel="manifest" href={`${site.url}site.webmanifest`} />
+    href={`${site.url}/favicon/favicon-16x16.png`}
+  />
 </svelte:head>
 
 <slot />
