492c7f9a895321a1d0985d91809a2e1b329673f0
Christopher Hicks <hickscme@gmail.com>
2024-01-21 10:14:18 -0800

    set img height

--- src/lib/ReviewLinks.svelte
+++ src/lib/ReviewLinks.svelte
@@ -5,7 +5,7 @@
 {#each site.socials as social (social.id)}
   {#if social.reviewLink && social.reviewText}
     <p class="mt-4 text-xl flex flex-wrap items-center gap-2">
-      <img class="h-6 inline-block" src={social.icon} alt={social.iconAlt} />
+      <img class="h-6 inline-block aspect-square" src={social.icon} alt={social.iconAlt} />
       <a target="_blank" class="link p-2" href={social.reviewLink}>
         {social.reviewText}
       </a>
